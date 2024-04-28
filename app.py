cb914c0ad423c13cca2f8e5ea9694434b930b58e
Farhan Ali <farhanyounas5204@gmail.com>
2024-04-28 22:36:59 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,64 +1,54 @@
 import streamlit as st
 import pickle
-import pandas as pd
-import requests
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
 
-def fetch_poster(movie_id):
-    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
-    data = response.json()
-    print(data)
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+ps = PorterStemmer()
 
 
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
 
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
 
+    text = y[:]
+    y.clear()
 
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
 
+    text = y[:]
+    y.clear()
 
+    for i in text:
+        y.append(ps.stem(i))
 
-def Recommend(movie):
-     movie_index = movies[movies['title'] == movie].index[0]
-     distances = similarity[movie_index]
-     movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x: x[1])[1:6]
-     recommended_movies = []
-     recommended_movies_posters = []
-    
-     for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id
-       
-        recommended_movies.append(movies.iloc[i[0]].title)
-         #fetch poster from API
-        recommended_movies_posters.append(fetch_poster(movie_id))
-     return recommended_movies, recommended_movies_posters
+    return " ".join(y)
 
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
-movies_dict = pickle.load(open("movie_dict.pkl","rb"))
-movies = pd.DataFrame(movies_dict)
+st.title("Email/SMS Spam Classifier")
 
+input_sms = st.text_area("Enter the message")
 
-similarity = pickle.load(open("similarity.pkl","rb"))
+if st.button('Predict'):
 
-st.title("Movie Recommender System")
-
-selected_movie_name = st.selectbox('Select the movie', movies["title"].values)
-
-if st.button("Recommend"):
-    names, posters = Recommend(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-
-    
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
