b996ca35f5e38db3a58b9931d912c6a0a6c76cbd
Farhan Ali <farhanyounas5204@gmail.com>
2024-09-28 22:16:32 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -2,11 +2,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/Home';
-import Signup from './src/Signup';
-import Login from './src/Login';
 import CameraScreen from './src/CameraScreen';
-import ButtonHome from './src/ButtonHome';
 import Summary from './src/Summary'; 
+import FontLoader from './src/FontLoader';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,13 +13,31 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="CameraScreen" component={CameraScreen} />
         <Stack.Screen name="Summary" component = {Summary}/>
-      </Stack.Navigator>
+        <Stack.Screen name="FontLoader" component={FontLoader}/>
+
+    
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
